43da937a3b527436262dcc02764c3bb50e99bebc
vicky <srivicky2000@gmail.com>
2022-10-21 12:13:47 +0530

    working on: product lisrings

--- src/wrappers/product/ShopSidebar.js
+++ src/wrappers/product/ShopSidebar.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   getIndividualCategories,
   getIndividualTags,
@@ -12,6 +12,9 @@ import ShopColor from "../../components/product/ShopColor";
 import ShopSize from "../../components/product/ShopSize";
 import ShopTag from "../../components/product/ShopTag";
 import { categoryData } from "../../pages/shop-product/Category";
+import { productData } from "../../data-helper/product";
+import { setActiveSort } from "../../helpers/product";
+
 
 const ShopSidebar = ({ products, getSortParams, sideSpaceClass }) => {
   const uniqueCategories = getIndividualCategories(products);
@@ -19,16 +22,40 @@ const ShopSidebar = ({ products, getSortParams, sideSpaceClass }) => {
   const uniqueSizes = getProductsIndividualSizes(products);
   const uniqueTags = getIndividualTags(products);
 
-  const selectedCategory = () => {
-    
+
+  const [selectedCategoryHere, setselectedCategoryHere] = useState('');
+  const selectedCategory = (category) => {
+      console.log(category);
+      setselectedCategoryHere(category);
   }
+
+  const getProductByCategory = () => {
+
+  }
+
+  const cycleSize = [
+    '4`10”-5`0” - 148-152 cm',
+    '5`0″-5`3″  - 152-160 cm',
+    '5`3″-5`6″  - 160-168 cm',
+    '5`6″-5`9″  - 168-175 cm',
+]
 
 
   return (
     <div className={`sidebar-style ${sideSpaceClass ? sideSpaceClass : ""}`}>
       {/* shop search */}
-      <ShopSearch />
-
+      {/* <ShopSearch /> */}
+      <div className="sidebar-widget">
+      <h4 className="pro-sidebar-title">Search </h4>
+      <div className="pro-sidebar-search mb-50 mt-25">
+        <form className="pro-sidebar-search-form" action="#">
+          <input type="text" placeholder="Search here..." />
+          <button>
+            <i className="pe-7s-search" />
+          </button>
+        </form>
+      </div>
+    </div>
       {/* filter by categories */}
       <>
       <div className="sidebar-widget">
@@ -39,9 +66,6 @@ const ShopSidebar = ({ products, getSortParams, sideSpaceClass }) => {
             <li>
               <div className="sidebar-widget-list-left">
                 <button
-                  onClick={e => {
-                    
-                  }}
                 >
                   <span className="checkmark" /> All Categories
                 </button>
@@ -52,9 +76,7 @@ const ShopSidebar = ({ products, getSortParams, sideSpaceClass }) => {
                 <li key={key}>
                   <div className="sidebar-widget-list-left">
                     <button
-                      onClick={e => {
-                        
-                      }}
+                      onClick={() => selectedCategory(category)}
                     >
                       {" "}
                       <span className="checkmark" /> {category.name}{" "}
@@ -76,7 +98,7 @@ const ShopSidebar = ({ products, getSortParams, sideSpaceClass }) => {
        
         
       </div>
-    </div>
+      </div>
       </>
       {/* <ShopCategories
         categories={uniqueCategories}
@@ -84,11 +106,57 @@ const ShopSidebar = ({ products, getSortParams, sideSpaceClass }) => {
       /> */}
 
       {/* filter by color */}
-      <ShopColor colors={uniqueColors} getSortParams={getSortParams} />
+      {/* <ShopColor colors={uniqueColors} getSortParams={getSortParams} /> */}
 
       {/* filter by size */}
-      <ShopSize sizes={uniqueSizes} getSortParams={getSortParams} />
-
+      {/* <ShopSize sizes={uniqueSizes} getSortParams={getSortParams} /> */}
+      <>
+      <div className="sidebar-widget mt-40">
+      <h4 className="pro-sidebar-title">Size </h4>
+      <div className="sidebar-widget-list mt-20">
+        {cycleSize ? (
+            <ul>
+            <li>
+              <div className="sidebar-widget-list-left">
+                <button
+                  onClick={e => {
+                    getSortParams("size", "");
+                    setActiveSort(e);
+                  }}
+                >
+                  <span className="checkmark" /> All Sizes{" "}
+                </button>
+              </div>
+            </li>
+            {cycleSize.map((size, key) => {
+              return (
+                <li key={key}>
+                  <div className="sidebar-widget-list-left">
+                    <button
+                      className="text-uppercase"
+                      onClick={e => {
+                        getSortParams("size", size);
+                        setActiveSort(e);
+                      }}
+                    >
+                      {" "}
+                      <span className="checkmark" />
+                      {size}{" "}
+                    </button>
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          "No sizes found"
+        )
+        }
+       
+      </div>
+    </div>
+      
+      </>
       {/* filter by tag */}
       {/* <ShopTag tags={uniqueTags} getSortParams={getSortParams} /> */}
     </div>
